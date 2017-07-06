a5e228b521a53e15f0cbb8ab91807c5d3837057f
Arpit Gupta <arpit.gupta.contractor2@hilton.com>
2017-07-05 20:13:48 -0500

    Intial commit for Redux Todo App

---
+++ src/js/containers/VisibleTodoList.js
@@ -0,0 +1,36 @@
+import { connect } from 'react-redux';
+import {VisibilityFilters, toggleTodo} from '../actions/index';
+import TodoList from '../components/TodoList';
+
+const { SHOW_ALL, SHOW_COMPLETED, SHOW_ACTIVE } = VisibilityFilters;
+
+
+
+const getVisibleTodos = ( todos, filter) => {
+  switch(filter){
+    case SHOW_ALL:
+      return todos;
+    case SHOW_COMPLETED:
+      return todos.filter( todo => todo.completed);
+    case SHOW_ACTIVE:
+      return todos.filter( todo => !todo.completed);
+  }
+};
+
+const mapStateToProps = state => {
+  return {
+    todos: getVisibleTodos(state.todos, state.visibilityFilter)
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onTodoClick: id => {
+      dispatch(toggleTodo(id))
+    }
+  }
+}
+
+const VisibleTodoList = connect(mapStateToProps, mapDispatchToProps)(TodoList)
+
+export default VisibleTodoList
---
+++ src/js/reducers/todos-reducer.js
@@ -0,0 +1,18 @@
+import { ADD_TODO, TOGGLE_TODO } from '../actions/index';
+
+export default function todos(state = [], action){
+  switch(action.type){
+    case ADD_TODO:
+      return [ ...state, {text: action.text, completed: false, id: state.length}];
+    case TOGGLE_TODO:
+      return state.map( (todo, index) => {
+          if(index === action.index) {
+            return Object.assign({}, todo, { completed: !todo.completed })
+          }else{
+            return todo
+          }
+        });
+    default:
+      return state;
+  }
+}
